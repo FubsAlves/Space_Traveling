3fcb65a2b3a11d319367b8a5e8dab6439fee3b99
Fabiano Alves <fabianoxtx@gmail.com>
2022-06-15 05:18:46 -0300

    Map de Posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -34,18 +35,24 @@ export default function Home(homeProps : HomeProps) {
     <Head>
       <title>SpaceTraveling | Home</title>
     </Head>
+    {console.log(homeProps.postsPagination.results)}
     <div className={commonStyles.container}>
       <main className={styles.content}>
         <div className={styles.logo}>
           <Image src='/logo.svg' width={225} layout='fixed' height={30} alt='logo'/>
         </div>
         <div className={styles.posts}>
-            <article>
-                <strong>Nam fringilla orci in finibus vestibulum</strong>
-                <span>Nunc nec orci in ex gravida rutrum. Duis eu dui accumsan sem cursus hendrerit vitae in augue.</span>
-                <time><FiCalendar /> 15/06/2022</time>
-                <cite><FiUser /> Maecenas</cite>
-            </article>
+          {homeProps.postsPagination.results.map(post => (
+              <Link key={post.uid} href={`/posts/${post.uid}`}>
+              <a href="#">
+                <strong>{post.data.title}</strong>
+                <span>{post.data.subtitle}</span>
+                <time><FiCalendar /> {post.first_publication_date}</time>
+                <cite><FiUser /> {post.data.author}</cite>
+              </a>
+              </Link> 
+          ))}
+            
         </div>
       </main>
     </div>
@@ -55,11 +62,11 @@ export default function Home(homeProps : HomeProps) {
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient({});
-  const postsResponse = await prismic.getByType('posts', {pageSize: 10});
+  const postsPagination = await prismic.getByType('posts', {pageSize: 10});
 
   return {
     props: {
-      postsResponse
+      postsPagination
     },
   }
 };
